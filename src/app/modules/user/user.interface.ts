4d5d6fcd135313c370f554e624e7e316a9ae2b55
MukitHossen7 <hossenmukit7@gmail.com>
2025-07-08 21:37:21 +0600

    user interface is created

---
+++ src/app/modules/user/user.interface.ts
@@ -0,0 +1,34 @@
+import { Types } from "mongoose";
+
+export enum Role {
+  ADMIN = "ADMIN",
+  USER = "USER",
+  GUIDE = "GUIDE",
+  SUPER_ADMIN = "SUPER_ADMIN",
+}
+
+export interface IAuthsProviders {
+  provider: string; //email, google
+  providerID: string;
+}
+
+export enum IsActive {
+  ACTIVE = "ACTIVE",
+  INACTIVE = "INACTIVE",
+  BLOCKED = "BLOCKED",
+}
+export interface IUser {
+  name: string;
+  email: string;
+  password?: string;
+  phone?: string;
+  picture?: string;
+  address?: string;
+  isDeleted?: boolean;
+  isActive?: IsActive;
+  isVerified?: boolean;
+  auths: IAuthsProviders[];
+  role: Role;
+  booking?: Types.ObjectId[];
+  guides?: Types.ObjectId[];
+}
