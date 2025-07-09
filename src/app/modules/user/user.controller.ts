a7e9d1255efe26de44a02e19b5f03bfa317db594
MukitHossen7 <hossenmukit7@gmail.com>
2025-07-10 00:13:17 +0600

    create send respon using for handle respons professionly

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -2,13 +2,15 @@
 import { NextFunction, Request, Response } from "express";
 import { userServices } from "./user.service";
 import { catchAsync } from "../../utils/catchAsync";
-
+import { sendResponse } from "../../utils/sendResponse";
+import httpStatus from "http-status-codes";
 const createUser = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
     const user = await userServices.createUser(req.body);
-    res.status(201).json({
+    sendResponse(res, {
+      statusCode: httpStatus.CREATED,
       success: true,
-      message: "User created successfully",
+      message: "User Created Successfully",
       data: user,
     });
   }
@@ -17,9 +19,10 @@ const createUser = catchAsync(
 const getAllUsers = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
     const user = await userServices.getAllUsers();
-    res.status(201).json({
+    sendResponse(res, {
+      statusCode: httpStatus.OK,
       success: true,
-      message: "User retrieve successfully",
+      message: "User Retrieved Successfully",
       data: user,
     });
   }
