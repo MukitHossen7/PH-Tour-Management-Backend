6e709113a6d2f3dc5470c7fddb422a6e2b362415
MukitHossen7 <hossenmukit7@gmail.com>
2025-07-10 23:12:11 +0600

    use zod validation for create new user

--- src/app/modules/user/user.routes.ts
+++ src/app/modules/user/user.routes.ts
@@ -1,9 +1,15 @@
 import express from "express";
 import { userControllers } from "./user.controller";
+import { zodValidateRequest } from "../../middlewares/zodValidateRequest";
+import { createUserZodSchema } from "./user.zod.validation";
 
 const userRoute = express.Router();
 
-userRoute.post("/register", userControllers.createUser);
+userRoute.post(
+  "/register",
+  zodValidateRequest(createUserZodSchema),
+  userControllers.createUser
+);
 userRoute.get("/all-users", userControllers.getAllUsers);
 
 export default userRoute;
