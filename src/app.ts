305b18e86f7b152872356be6328ace6bbef0f626
MukitHossen7 <hossenmukit7@gmail.com>
2025-07-13 23:31:17 +0600

    feat(auth): add passport and express-session for authentication and session management

--- src/app.ts
+++ src/app.ts
@@ -5,10 +5,23 @@ import routes from "./app/routes/routes";
 import { globalErrorHandler } from "./app/middlewares/globalErrorHandler";
 import notFound from "./app/middlewares/notFound";
 import cookieParser from "cookie-parser";
+import passport from "passport";
+import expressSession from "express-session";
 export const app = express();
 
 //middleware
-app.use([cookieParser(), express.json(), cors()]);
+app.use(
+  expressSession({
+    secret: "Your secret",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(cookieParser());
+app.use(express.json());
+app.use(cors());
 
 //routes
 app.use("/api/v1", routes);
--- src/config/index.ts
+++ src/config/index.ts
@@ -12,4 +12,9 @@ export default {
   super_admin_email: process.env.SUPER_ADMIN_EMAIL,
   jwt_refresh_secret: process.env.JWT_REFRESH_SECRET,
   jwt_refresh_expiration: process.env.JWT_REFRESH_EXPIRATION,
+  GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET as string,
+  GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID as string,
+  GOOGLE_CALLBACK_URL: process.env.GOOGLE_CALLBACK_URL as string,
+  EXPRESS_SESSION_SECRET: process.env.EXPRESS_SESSION_SECRET as string,
+  FRONTEND_URL: process.env.FRONTEND_URL as string,
 };
