43b64651c851299f3252639a1b8c97a50707a52c
MukitHossen7 <hossenmukit7@gmail.com>
2025-07-22 14:41:04 +0600

    create tour routes

---
+++ src/app/modules/tour/tour.routes.ts
@@ -0,0 +1,61 @@
+import express from "express";
+import { TourController } from "./tour.controller";
+import { checkAuth } from "../../middlewares/checkAuth";
+import { Role } from "../user/user.interface";
+import { zodValidateRequest } from "../../middlewares/zodValidateRequest";
+import {
+  createTourTypeZodSchema,
+  createTourZodSchema,
+  updateTourZodSchema,
+} from "./tour.zod.validation";
+
+const tourRoute = express.Router();
+
+// TOUR TYPE ROUTES
+
+tourRoute.get("/tour-types", TourController.getAllTourTypes);
+
+tourRoute.post(
+  "/create-tour-type",
+  checkAuth(Role.ADMIN, Role.SUPER_ADMIN),
+  zodValidateRequest(createTourTypeZodSchema),
+  TourController.createTourType
+);
+
+tourRoute.patch(
+  "/tour-types/:id",
+  checkAuth(Role.ADMIN, Role.SUPER_ADMIN),
+  zodValidateRequest(createTourTypeZodSchema),
+  TourController.updateTourType
+);
+
+tourRoute.delete(
+  "/tour-types/:id",
+  checkAuth(Role.ADMIN, Role.SUPER_ADMIN),
+  TourController.deleteTourType
+);
+
+//TOUR ROUTES
+// tourRoute.get("/", TourController.getAllTours);
+
+tourRoute.post(
+  "/create",
+  checkAuth(Role.ADMIN, Role.SUPER_ADMIN),
+  zodValidateRequest(createTourZodSchema),
+  TourController.createTour
+);
+
+tourRoute.patch(
+  "/:id",
+  checkAuth(Role.ADMIN, Role.SUPER_ADMIN),
+  zodValidateRequest(updateTourZodSchema),
+  TourController.updateTour
+);
+
+tourRoute.delete(
+  "/:id",
+  checkAuth(Role.ADMIN, Role.SUPER_ADMIN),
+  TourController.deleteTour
+);
+
+export default tourRoute;
--- src/app/routes/routes.ts
+++ src/app/routes/routes.ts
@@ -2,11 +2,13 @@ import { Router } from "express";
 import userRoute from "../modules/user/user.routes";
 import authRoute from "../modules/auth/auth.routes";
 import divisionRoute from "../modules/division/division.routes";
+import tourRoute from "../modules/tour/tour.routes";
 
 const routes = Router();
 
 routes.use("/user", userRoute);
 routes.use("/auth", authRoute);
 routes.use("/division", divisionRoute);
+routes.use("/tour", tourRoute);
 
 export default routes;
