867548573b4ed6d14d6387bd3298789685496087
MukitHossen7 <hossenmukit7@gmail.com>
2025-07-22 11:39:33 +0600

    division services crud funtionlity is complete

--- src/app/modules/division/division.service.ts
+++ src/app/modules/division/division.service.ts
@@ -11,6 +11,54 @@ const createDivision = async (payload: IDivision) => {
   return division;
 };
 
+const getAllDivisions = async () => {
+  const divisions = await Division.find({});
+  const totalDivision = await Division.countDocuments();
+  return {
+    data: divisions,
+    meta: {
+      total: totalDivision,
+    },
+  };
+};
+
+const getSingleDivision = async (slug: string) => {
+  const division = await Division.findOne({ slug: slug });
+
+  return division;
+};
+
+const updateDivision = async (id: string, payload: Partial<IDivision>) => {
+  const existingDivision = await Division.findById(id);
+  if (!existingDivision) {
+    throw new Error("Division not found.");
+  }
+
+  const duplicateDivision = await Division.findOne({
+    name: payload.name,
+    _id: { $ne: id },
+  });
+  if (duplicateDivision) {
+    throw new Error("A division with this name already exists.");
+  }
+
+  const updateDivision = await Division.findByIdAndUpdate(id, payload, {
+    new: true,
+    runValidators: true,
+  });
+
+  return updateDivision;
+};
+
+const deleteDivision = async (id: string) => {
+  await Division.findByIdAndDelete(id);
+  return null;
+};
+
 export const DivisionService = {
   createDivision,
+  getAllDivisions,
+  getSingleDivision,
+  updateDivision,
+  deleteDivision,
 };
