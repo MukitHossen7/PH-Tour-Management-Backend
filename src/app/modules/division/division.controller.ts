53d7d7254c3d26f4d4eefb9624dffdbe4b426ebb
MukitHossen7 <hossenmukit7@gmail.com>
2025-07-22 12:00:30 +0600

    division controller all functionity is done

--- src/app/modules/division/division.controller.ts
+++ src/app/modules/division/division.controller.ts
@@ -18,6 +18,66 @@ const createDivision = catchAsync(
   }
 );
 
+const getAllDivisions = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const result = await DivisionService.getAllDivisions();
+
+    sendResponse(res, {
+      statusCode: httpStatus.OK,
+      success: true,
+      message: "Division retrieve successfully",
+      data: result.data,
+      meta: result.meta,
+    });
+  }
+);
+
+const getSingleDivision = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const slug = req.params.slug;
+    const result = await DivisionService.getSingleDivision(slug);
+
+    sendResponse(res, {
+      statusCode: httpStatus.OK,
+      success: true,
+      message: "Get single division successfully",
+      data: result,
+    });
+  }
+);
+
+const updateDivision = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const id = req.params.id;
+    const result = await DivisionService.updateDivision(id, req.body);
+
+    sendResponse(res, {
+      statusCode: httpStatus.OK,
+      success: true,
+      message: "Division Update successfully",
+      data: result,
+    });
+  }
+);
+
+const deleteDivision = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const id = req.params.id;
+    const result = await DivisionService.deleteDivision(id);
+
+    sendResponse(res, {
+      statusCode: httpStatus.OK,
+      success: true,
+      message: "Division deleted successfully",
+      data: result,
+    });
+  }
+);
+
 export const DivisionController = {
   createDivision,
+  getAllDivisions,
+  getSingleDivision,
+  updateDivision,
+  deleteDivision,
 };
