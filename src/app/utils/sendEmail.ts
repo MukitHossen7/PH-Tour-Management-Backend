2e09b09413bfa094b52775761f8bef70aa2b5aac
MukitHossen7 <hossenmukit7@gmail.com>
2025-07-28 11:26:44 +0600

    crate nodemailer setup

---
+++ src/app/utils/sendEmail.ts
@@ -0,0 +1,57 @@
+/* eslint-disable no-console */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import nodemailer from "nodemailer";
+import config from "../../config";
+import path from "path";
+import ejs from "ejs";
+import AppError from "../errorHelpers/AppError";
+
+const transporter = nodemailer.createTransport({
+  host: config.EMAIL_SENDER.SMTP_HOST,
+  port: Number(config.EMAIL_SENDER.SMTP_PORT),
+  secure: true,
+  auth: {
+    user: config.EMAIL_SENDER.SMTP_USER,
+    pass: config.EMAIL_SENDER.SMTP_PASS,
+  },
+});
+
+interface sendEmailResponse {
+  to: string;
+  subject: string;
+  templateName: string;
+  templateData?: Record<string, any>;
+  attachments?: {
+    filename: string;
+    content: Buffer | string;
+    contentType: string;
+  }[];
+}
+
+export const sendEmail = async ({
+  to,
+  subject,
+  templateName,
+  templateData,
+  attachments,
+}: sendEmailResponse) => {
+  try {
+    const templatePath = path.join(__dirname, `templates/${templateName}.ejs`);
+    const html = await ejs.renderFile(templatePath, templateData);
+    const info = await transporter.sendMail({
+      from: config.EMAIL_SENDER.SMTP_FROM,
+      to: to,
+      subject: subject,
+      html: html,
+      attachments: attachments?.map((attachment) => ({
+        filename: attachment.filename,
+        content: attachment.content,
+        contentType: attachment.contentType,
+      })),
+    });
+    console.log(`\u2709\uFE0F Email sent to ${to}: ${info.messageId}`);
+  } catch (error: any) {
+    console.log("email sending error", error.message);
+    throw new AppError(401, "Email error");
+  }
+};
